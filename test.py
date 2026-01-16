2383fc6a75465655a7b088413cf98f441b512cd4
kzl5 <kzl5@scarletmail.rutgers.edu>
2026-01-15 23:26:22 -0500

    dynamic sim demo and adv. setting addition

--- test.py
+++ test.py
@@ -2,13 +2,26 @@ import genesis as gs
 import numpy as np
 ########################################init
 print(gs.__path__) # check if this is as expected
-gs.init(backend=gs.cpu)
+gs.init(backend=gs.cuda)
 
 #######################################scene create
-scene = gs.Scene(show_viewer=True, sim_options=gs.options.SimOptions(dt = 0.01,))
+scene = gs.Scene(
+    show_viewer=True, 
+    sim_options=gs.options.SimOptions(
+        dt = 0.01,
+        gravity = (0, 0, -9.81)
+        )
+    )
+
+####plane
 plane = scene.add_entity(gs.morphs.Plane())
 franka = scene.add_entity(
-    gs.morphs.MJCF(file='xml/universal_robots_ur5e/ur5e.xml'),
+    gs.morphs.MJCF(
+        file='xml/universal_robots_ur5e/ur5e.xml',
+        pos = (0,0,0),
+        euler = (0,0,0),
+        scale = 1.0
+        )
 )
 
 scene.build()
